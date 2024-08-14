5c92b9582e177496a4ee5ddf664beaa17dd82ded
Subhita <37235520+subhita@users.noreply.github.com>
2024-08-14 15:22:36 +0000

    added in ternet gateway, route table, security group and S3, ec2 server

--- VPC_with_EC2/main.tf
+++ VPC_with_EC2/main.tf
@@ -18,3 +18,80 @@ resource "aws_subnet" "sub2" {
   availability_zone = "us-east-1b"
   map_public_ip_on_launch = true
 }
+
+resource "aws_internet_gateway" "aig" {
+  vpc_id = aws_vpc.myvpc.id
+}
+
+resource "aws_route_table" "RT" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route = {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.aig.id
+  }
+}
+
+resource "aws_route_table_association" "rta1" {
+  subnet_id = aws_subnet.sub1.id
+  route_table_id = aws_route_table.RT
+  
+}
+
+resource "aws_route_table_association" "rta2" {
+  subnet_id = aws_subnet.sub2.id
+  route_table_id = aws_route_table.RT
+  
+}
+
+resource "aws_security_group" "webSg" {
+  name = "web"
+  vpc_id = aws_vpc.myvpc.id 
+
+  ingress = {
+    description = "HTTP from VPC"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_block = ["0.0.0.0/0"]
+  }
+
+  #  ingress = {
+  #   description = "SSH"
+  #   from_port = 22
+  #   to_port = 22
+  #   protocol = "tcp"
+  #   cidr_block = ["0.0.0.0/0"]
+  # }
+
+    egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Web-sg"
+  }
+  }
+
+  resource "aws_s3_bucket" "example" {
+    bucket = "subhita_terraform_project"
+  }
+
+resource "aws_instance" "webserver1" {
+   ami                    = "ami-04a81a99f5ec58529"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.webSg.id]
+  subnet_id              = aws_subnet.sub1.id
+  user_data              = base64encode(file("userdata.sh"))
+}
+
+resource "aws_instance" "webserver2" {
+   ami                    = "ami-04a81a99f5ec58529"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.webSg.id]
+  subnet_id              = aws_subnet.sub2.id
+  user_data              = base64encode(file("userdata1.sh"))
+}
